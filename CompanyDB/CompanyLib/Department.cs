3ac9a1f740fde9a116b88ea7dcad5c939bb86bc5
karimkohel <kareemkohel@gmail.com>
2020-12-23 23:20:03 +0200

    added solution with base classes in separate Lib project because we Agile

---
+++ CompanyDB/CompanyLib/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLib
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Address { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+    }
+}
---
+++ CompanyDB/CompanyLib/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLib
+{
+    class Employee
+    {
+        public String FisrtName { get; set; }
+        public String LastName { get; set; }
+        public char Minit { get; set; }
+        public long SSN { get; set; }
+        public string Birthday { get; set; } // learn dates in C#
+        public string Address { get; set; }
+        public char Sex { get; set; }
+        public double Salary { get; set; }
+        public int DepartmentNumber { get; set; }
+
+    }
+
+}

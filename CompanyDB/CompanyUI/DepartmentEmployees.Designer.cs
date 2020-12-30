7f91ec173fbf7375bc8af9ed7456b56a61737539
karimkohel <kareemkohel@gmail.com>
2020-12-30 23:57:55 +0200

    added basic form depemployees form with basic settings

---
+++ CompanyDB/CompanyUI/DepartmentEmployees.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace CompanyUI
+{
+    partial class DepartmentEmployees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DepartmentEmployees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(501, 611);
+            this.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "DepartmentEmployees";
+            this.Text = "DepartmentEmployees";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
